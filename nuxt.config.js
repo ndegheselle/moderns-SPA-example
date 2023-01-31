cd11a2753bbb58c1abb527dd557f7599f4925379
Poulpynateur <nicolas@degheselle.fr>
2023-01-31 22:12:41 +0100

    Basic nuxt config

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: [
+        '@primer/css/index.scss'
+    ],
+    modules: [
+        '@pinia/nuxt',
+    ],
+})
