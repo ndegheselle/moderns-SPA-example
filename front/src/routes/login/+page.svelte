51cee93327920b24ec6f21a43136dd329de217ba
Poulpynateur <nicolas@degheselle.fr>
2023-02-16 22:48:42 +0100

    Login page

---
+++ front/src/routes/login/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+    import { PUBLIC_API_URL } from "$env/static/public";
+
+    let haveError = false;
+
+    let username = "";
+    let password = "";
+
+    function login() {
+        fetch(`${PUBLIC_API_URL}/auth/login`, {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        })
+            .then(async function (response) {
+                if (!response.ok) throw new Error(response.status);
+
+                const user = await response.json();
+                console.log(user);
+            })
+            .catch(function (error) {
+                haveError = true;
+            });
+    }
+</script>
+
+<div class="login-container">
+    <div class="card login-card">
+        <header class="card-header">
+            <p class="card-header-title">Login</p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                <div class="field">
+                    <label class="label"
+                        >Username
+                        <div class="control">
+                            <input
+                                class="input"
+                                class:is-danger={haveError}
+                                type="text"
+                                bind:value={username}
+                            />
+                        </div>
+                    </label>
+                </div>
+                <div class="field">
+                    <label class="label"
+                        >Password
+                        <div class="control">
+                            <input
+                                class="input"
+                                class:is-danger={haveError}
+                                type="password"
+                                bind:value={password}
+                            />
+                            <p class="help is-danger" class:is-hidden={!haveError}>Invalid username or password.</p>
+                        </div>
+                    </label>
+                </div>
+
+                <div class="control">
+                    <button class="button is-primary" on:click={login}
+                        >Login</button
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style scoped>
+    .login-container {
+        min-height: 100vh;
+        min-width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .login-card {
+        width: 30rem;
+    }
+</style>
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,14 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-// Proxy : https://stackoverflow.com/questions/72753092/how-to-proxy-on-svelte-kit-in-dev-mode
-// Dev : https://dev.to/xjamundx/adding-a-rest-api-to-your-vite-server-in-5-seconds-270g
-
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-        proxy: {
-            '/api': 'http://localhost:3000',
-        },
-    }
+	plugins: [sveltekit()]
 });
